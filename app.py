624feddcc32308985bb0f324bebc9de6e0967333
alfmorais <alfredneto@hotmail.com>
2023-05-24 19:18:56 -0300

    Começo da implementação do token JWT

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import os
 
 from database import db
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_smorest import Api
-from models import Item, ItemTags, Store, Tag  # noqa F401
+from models import Item, ItemTags, Store, Tag, User  # noqa F401
 from resources.items import blp as ItemsBlueprint
 from resources.stores import blp as StoresBlueprint
 from resources.tags import blp as TagsBlueprint
@@ -35,8 +36,14 @@ def create_app(db_url=None):
     with app.app_context():
         db.create_all()
 
-    # Blueprints configuration
+    # Blueprints
     api = Api(app)
+
+    # JWT Configuration
+    app.config["JWT_SECRETY_KEY"] = "84466196-47cb-4bdb-8b2a-f4a157cc34fe"
+    jwt = JWTManager(app)  # noqa F841
+
+    # Blueprints configuration
     api.register_blueprint(ItemsBlueprint)
     api.register_blueprint(StoresBlueprint)
     api.register_blueprint(TagsBlueprint)
--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from models.items import Item  # noqa F401
 from models.items_tags import ItemTags  # noqa F401
 from models.stores import Store  # noqa F401
 from models.tags import Tag  # noqa F401
+from models.users import User  # noqa F401
